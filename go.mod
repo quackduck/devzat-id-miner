5cabe08435267b66ee9c45b215b326b9193aecc2
Ishan Goel <igoel.mail@gmail.com>
2023-03-18 13:54:55 -0700

    stuff

---
+++ go.mod
@@ -0,0 +1,15 @@
+module mineid
+
+go 1.20
+
+require (
+	github.com/caarlos0/sshmarshal v0.1.0
+	github.com/fatih/color v1.15.0
+	golang.org/x/crypto v0.7.0
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"crypto/ed25519"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/pem"
+	"fmt"
+	"math/big"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/caarlos0/sshmarshal"
+	"github.com/fatih/color"
+	"golang.org/x/crypto/ssh"
+)
+
+var (
+	prefix     = ""
+	numThreads = 1
+)
+
+func main() {
+	// get first argument and set prefix to that
+	if len(os.Args) > 2 {
+		prefix = os.Args[1]
+		var err error
+		numThreads, err = strconv.Atoi(os.Args[2])
+		if err != nil {
+			panic(err)
+		}
+	} else if len(os.Args) > 1 {
+		prefix = os.Args[1]
+	} else {
+		// print help message
+		fmt.Println("error: need at least one argument.\nUsage:\n   ./mineid <prefix> [<num of threads>]")
+		return
+	}
+
+	trimmed := prefix
+	for _, r := range "0123456789abcdef" {
+		trimmed = strings.ReplaceAll(trimmed, string(r), "")
+	}
+	// is there a more efficient way to do this?
+	if trimmed != "" {
+		if len(trimmed) == 1 {
+			fmt.Println("error: prefix contains this non-hex character: " + trimmed)
+		} else {
+			fmt.Println("error: prefix contains these non-hex characters: " + trimmed)
+		}
+		return
+	}
+
+	done := make(chan bool)
+	for i := 0; i < numThreads; i++ {
+		go worker(done)
+	}
+	<-done
+}
+
+func worker(done chan bool) {
+	i := bytesToBigint(getRandBytes32())
+	key := make([]byte, 32)
+	oneAsBigint := big.NewInt(1)
+	for {
+		priv, sshpub, id := genKey(i.FillBytes(key))
+		if strings.HasPrefix(id, prefix) {
+			fmt.Print("Found key with id: ")
+			color.Yellow(id)
+			//fmt.Println(base64.StdEncoding.EncodeToString(priv))
+			//pem.Encode(os.Stdout, &pem.Block{
+			//	Type:  "DEVZAT-GEN PRIVATE KEY",
+			//	Bytes: priv})
+			color.Yellow("\nPrivate key: ")
+			blk, err := sshmarshal.MarshalPrivateKey(priv, "")
+			if err != nil {
+				panic(err)
+			}
+			pem.Encode(os.Stdout, blk)
+			//pem.Encode(os.Stdout, &pem.Block{Type: "edkey", Bytes: edkey.MarshalED25519PrivateKey(priv)})
+			//pem.Encode(os.Stdout, &pem.Block{
+			//	Type:  "DEVZAT-GEN PUBLIC KEY",
+			//	Bytes: pub})
+			color.Yellow("\nPublic key: ")
+			fmt.Println(string(ssh.MarshalAuthorizedKey(sshpub)))
+			done <- true
+			break
+		}
+		i.Add(i, oneAsBigint)
+	}
+}
+
+func getRandBytes32() []byte {
+	b := make([]byte, 32)
+	_, err := rand.Read(b)
+	if err != nil {
+		panic(err)
+	}
+	return b
+}
+
+func bytesToBigint(b []byte) *big.Int {
+	return new(big.Int).SetBytes(b)
+}
+
+func genKey(s []byte) (privkey ed25519.PrivateKey, sshpubkey ssh.PublicKey, id string) {
+	priv := ed25519.NewKeyFromSeed(s)
+	pub := priv.Public().(ed25519.PublicKey)
+	//pub, priv, err := ed25519.GenerateKey(nil)
+	//if err != nil {
+	//	panic(err)
+	//}
+	sshPubKey, err := ssh.NewPublicKey(pub)
+	if err != nil {
+		panic(err)
+	}
+	return priv, sshPubKey, shasum(sshPubKey.Marshal())
+}
+
+func shasum(a []byte) string {
+	s := sha256.New()
+	s.Write(a)
+	return hex.EncodeToString(s.Sum(nil))
+}
